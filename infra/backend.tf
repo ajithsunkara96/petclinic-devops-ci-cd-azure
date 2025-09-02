a6d7634fcc4252fb13f073960440aff9f986b73f
ajithsunkara96 <ajithsunkara@gmail.com>
2025-09-02 19:52:50 -0400

    updated backend.tf with key

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "project7"
     storage_account_name = "project7storageacc"
-      container_name       = "tfstate"
-    }
+    container_name       = "tfstate"
+    key                  = "project7/infra.tfstate"
+  }
 }
