87eda5a1b4661892456cd11ca0c6c7d8a66ad035
ajithsunkara96 <ajithsunkara@gmail.com>
2025-09-02 19:46:55 -0400

    fixed backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,5 +2,6 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "project7"
     storage_account_name = "project7storageacc"
-    container_name       = "tfstate" }
+      container_name       = "tfstate"
+    }
 }
